1860f711cc9106e74f95a8647507f48b68c7dcb1
OwenSimmons0107 <140710436+OwenSimmons0107@users.noreply.github.com>
2023-08-31 20:11:31 +0100

    Update Combining_structures.py

--- Combining_structures.py
+++ Combining_structures.py
@@ -9,30 +9,18 @@ Created on Mon Jul 24 09:55:26 2023
 import SimpleITK as sitk
 import numpy as np
 
+#this code combines the separate masks into one single array
 
-patients = [
-#   'PT_3 (110850487)',
-#    'PT_4 (331844332)',
-#    'PT_9 (555118529)'
-#    '740810725'
-#    'PT_10 (596032881)'
-#    'PT_5 UIDQQ0X7axq0q1'
-#    'PT_2 UIDQQ0X70Q87a7'
-#    'PT_7 UIDQQ0X70X7AQ1'
-#    'PT_6 UIDQQ0X80hzruh'
-#    'PT_8 UIDQQ0X70Xhzr8'
-    'PT_1 UIDQQ0X1x1Hx7'
-]
+#populate list with patient names
+patients = []
 
 rois = ['dentition', 'ethmoid', 'mandible_l', 'mandible_r', 'maxilla_l', 'maxilla_r', 'nasal_bone', 'orbit_l', 'orbit_r', 'sphenoid_l', 'sphenoid_r', 'tmj_l', 'tmj_r']
 
-directory_path = "/Users/owensimmons/Desktop/repo for autosegmentation/output_mask_dir"
-output_path = "/Users/owensimmons/Desktop/repo for autosegmentation/nifti images/combined_mask_dir"
-
-
+directory_path = "path_to_mask_directory"
+output_path = "path_to_output_directory"
 
 for patient in patients:
-    ct_volume = sitk.ReadImage(f"/Users/owensimmons/Desktop/repo for autosegmentation/nifti images/output_ct_dir/{patient}.nii")
+    ct_volume = sitk.ReadImage(f"path_to_CT_images/{patient}")
     orig_spacing = ct_volume.GetSpacing()
     orig_origin = ct_volume.GetOrigin()
     ct_slices = sitk.GetArrayFromImage(ct_volume)
@@ -49,14 +37,6 @@ for patient in patients:
 
         # Add the current segmentation to the combined array
         segmentation_array += (idx + 1) * segmentation_slices
-
-
-    unique_values = np.unique(segmentation_array)
-
-    # The number of segmentations is the length of the unique_values array
-    num_segmentations = len(unique_values) - 1
-
-    print("Number of segmentations:", num_segmentations)
 
     combined_image =sitk.GetImageFromArray(segmentation_array)
     combined_image.SetSpacing(orig_spacing)

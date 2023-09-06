66245fc984918308bb88ca3dbee6f14fc384a760
OwenSimmons0107 <140710436+OwenSimmons0107@users.noreply.github.com>
2023-09-06 22:14:28 +0100

    Update resampling.py

--- resampling.py
+++ resampling.py
@@ -2,8 +2,10 @@ import SimpleITK as sitk
 import os
 import numpy as np
 
-input_dir = "nifti_images_cropped/ct_images"
-output_dir = "nifti_images_resampled"
+#this script resampled the images to work for training
+
+input_dir = ""
+output_dir = ""
 
 
 patients = [f for f in os.listdir(input_dir) if f.endswith(".nii")]
@@ -12,12 +14,11 @@ target_spacing = [1.50, 1.50, 3.0]  # Desired spacing
 
 for patient in patients:
     # Read the CT volume
-    ct_volume = sitk.ReadImage(os.path.join(input_dir, patient))
+    ct_volume = sitk.ReadImage(os.path.join(f"{input_dir}/CT_images", patient))
     print(ct_volume.GetSize())
 
     # Calculate target dimensions based on desired spacing
     target_dimensions = [int(np.ceil(ct_volume.GetSize()[i] * ct_volume.GetSpacing()[i] / spacing)) for i, spacing in enumerate(target_spacing)]
-    #print(target_dimensions)
     
     # Calculate the remainder when dividing by 8
     r = [dim % 8 for dim in target_dimensions]
@@ -35,16 +36,13 @@ for patient in patients:
     resampled_ct = sitk.Resample(ct_volume, target_dimensions, sitk.Transform(), sitk.sitkLinear,
                                  ct_volume.GetOrigin(), new_spacing, ct_volume.GetDirection(),
                                  0.0, ct_volume.GetPixelIDValue())
-    
-    print(resampled_ct.GetSize())
-    print(resampled_ct.GetSpacing())
 
     # Write the resampled CT volume to the output directory
     output_ct_path = os.path.join(output_dir, "ct_images", patient)
     sitk.WriteImage(resampled_ct, output_ct_path)
 
     # Similarly, resample the segmentation volume with the new spacing and dimensions
-    segmentation_volume = sitk.ReadImage(os.path.join("nifti_images_cropped/mask_images", patient))
+    segmentation_volume = sitk.ReadImage(os.path.join(f"{input_dir}/mask_images", patient))
     resampled_segmentation = sitk.Resample(segmentation_volume, target_dimensions, sitk.Transform(),
                                            sitk.sitkNearestNeighbor, segmentation_volume.GetOrigin(),
                                            new_spacing, segmentation_volume.GetDirection(),
